25b11004a983761bfe59233e2c17ff2618f014e3
Dan Souza <souza.music@outlook.com>
2022-03-27 13:42:18 -0700

    added date field and current day into p tag

--- Index.js
+++ Index.js
@@ -8,12 +8,16 @@ function weatherLoad(){
     fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
     .then(response => response.json())
     .then(data => { 
+        const date = data.init.split("", 8)
+        let year = date.slice(0,4) 
+        let day = date.slice(4,6)
+        let month = date.slice(6,8)
+        let newDate = (day.join("") + - + month.join("") + - + year.join(""))
         //const fiveDay = 
         //const oneDay = 
-        const twentyFourHour = data.dataseries;
-        const threeHour = twentyFourHour.splice(0, size);
-        let card = document.getElementsByClassName("card");
-        card.append(threeHour)
-          
+        const threeHour = data.dataseries[0];
+        let dataField = document.getElementById("weatherinfo");
+        dataField.append("Todays Date: ", newDate)
+          console.log(threeHour)
     })
 }
