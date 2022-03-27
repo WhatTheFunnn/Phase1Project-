8e37d6e614aba07ea4d09236366535c1e96319f3
Dan Souza <souza.music@outlook.com>
2022-03-27 15:01:08 -0700

    added temp fields for currrent and 3 hour, time display not showing full minutes

--- Index.js
+++ Index.js
@@ -4,20 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function weatherLoad(){
-    size = 3 
     fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
     .then(response => response.json())
-    .then(data => { 
+    .then(data => {
         const date = data.init.split("", 8)
         let year = date.slice(0,4) 
         let day = date.slice(4,6)
         let month = date.slice(6,8)
-        let newDate = (day.join("") + - + month.join("") + - + year.join(""))
-        //const fiveDay = 
-        //const oneDay = 
-        const threeHour = data.dataseries[0];
+        let newDate = (day.join("") + - + month.join("") + - + year.join("")) 
+        let today = new Date();
+        let twelveHourTime = ((today.getHours() + 13) % 12)
+        let  hrsAndMins = twelveHourTime + ":" + today.getMinutes()
+        
         let dataField = document.getElementById("weatherinfo");
-        dataField.append("Todays Date: ", newDate)
-          console.log(threeHour)
+        dataField.append("Todays Date: ", newDate, " " ,)
+        dataField.append(" Time: ", hrsAndMins, )
+
+
+        let current = data.dataseries[0]
+        let threeHour = data.dataseries[1]
+        let cloudCover = threeHour.cloudcover
+        let currentTemp = current.temp2m
+        let threeHourTemp = (threeHour.temp2m * 9/5 + 32)
+        console.log(currentTemp)
     })
 }
