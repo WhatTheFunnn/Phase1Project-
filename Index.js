a98f626975e23bc8a81cff9d0579deed9cef81a3
Dan Souza <souza.music@outlook.com>
2022-03-27 10:33:39 -0700

    created variables for 3hour 24hour 1 day and 5 day

--- Index.js
+++ Index.js
@@ -1,14 +1,19 @@
-let weatherData = []
-
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
     weatherLoad()
 })
 
 function weatherLoad(){
+    size = 3 
     fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
     .then(response => response.json())
-    .then(stats => 
-        console.log(stats.dataseries)
-    )
+    .then(data => { 
+        //const fiveDay = 
+        //const oneDay = 
+        const twentyFourHour = data.dataseries;
+        const threeHour = twentyFourHour.splice(0, size);
+        let card = document.getElementsByClassName("card");
+        card.append(threeHour)
+          
+    })
 }
