3f3b0720d559733b0b091329619a63d54c96108f
Dan Souza <souza.music@outlook.com>
2022-03-27 09:50:03 -0700

    passing json info into an array

--- Index.js
+++ Index.js
@@ -1,7 +1,14 @@
+let weatherData = []
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
+    weatherLoad()
 })
 
 function weatherLoad(){
-
+    fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
+    .then(response => response.json())
+    .then(stats => 
+        console.log(stats.dataseries)
+    )
 }
