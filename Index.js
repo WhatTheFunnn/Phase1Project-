730b9e43d0b57630ea19cd155b7c35dfb7f5793a
Dan Souza <souza.music@outlook.com>
2022-03-27 22:20:12 -0700

    added wind and gust elements to the data

--- Index.js
+++ Index.js
@@ -3,18 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     currentWeatherInfo()
 })
 
-function weatherLoad(){
-    fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
-    .then(response => response.json())
-    .then(data => {
-        let container = document.getElementById("container")
-        let newPTage = document.createElement("p")
-        container.append(newPTage)   
-        newPTage.append(currentTemp)    
-        console.log(container)
-    })
-}
-
 function currentWeatherInfo(){
     fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/colton%20OR?unitGroup=metric&key=D7EWQSWC4XASA23DCSMD9M4ZH&contentType=json")
     .then(response => response.json())
@@ -38,22 +26,32 @@ function currentWeatherInfo(){
             let temp = data.currentConditions.temp    
             let tempField = document.getElementById("temp")
             tempField.append("Temperature: ", temp * 9/5 + 32 + " F")
+            
             let conditions = data.currentConditions.conditions
             let condField = document.getElementById("conditions")
             condField.append("Condition: ", conditions)
+            
             let icon = data.currentConditions.icon
             let iconField = document.getElementById("icon")
             iconField.append(icon)
 
             let precipProb = data.currentConditions.precipprob
             let preciProbField = document.getElementById("preciprob")
-            preciProbField.append("Probability: ",precipProb)
+            preciProbField.append("Precipitation Probability: ",precipProb)
             
             let precip = data.currentConditions.precip
             let preciField = document.getElementById("preci")
-            preciField.append(precip)
+            preciField.append("Precipitation: ", precip)
+
+            let wind = data.currentConditions.windspeed
+            let windField = document.getElementById("wind")
+            windField.append("Wind Speed:", wind)
+
+            let gust = data.currentConditions.windgust
+            let gustField = document.getElementById("gust")
+            gustField.append("Gust Speed: ",gust)
 
             console.log(data)
-            console.log(precipProb)
+            console.log()
         })
     }
