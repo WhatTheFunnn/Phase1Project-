616ea57486dd81b63797451a01403417d0373359
Dan Souza <souza.music@outlook.com>
2022-03-30 01:26:44 -0700

    updating code on hourly function to parse correctly

--- Index.js
+++ Index.js
@@ -46,9 +46,11 @@ function currentWeatherInfo() {
             let gustField = document.getElementById("gust")
             gustField.append("Gust Speed: ", gust, " mph")
 
+
             time(data)
             locAndSearch(data)
             weekly(data)
+            // weatherBG(data)
             hourly(data)
         })
 };
@@ -107,90 +109,105 @@ function hourly(data) {
     let currentTemp = document.createElement("p")
     container.append(currentHour)
     container.append(currentTemp)
+
     if (hour) {
         for (let x = 0; x < hours.length; x++) {
             let newHour = hours[x]
-            if (hour == newHour.datetime.split(":")[0] < 11) {
-                let doubleDigit = newHour.datetime.split(":")[0]
-                currentHour.append("Time: ", hour % 12, " AM"), currentTemp.append("Temperature: ", hours[doubleDigit.split("")[1]].temp * 9 / 5 + 32 + " F")
-            }
-            else if (hour == newHour.datetime.split(":")[0]) {
-                currentHour.append("Time: ", hour % 12, " PM"), currentTemp.append("Temperature: ", hours[hour].temp * 9 / 5 + 32 + " F")
+
+            if (hour === newHour.datetime.split(":")[0] && hour < 12) {
+                currentHour.innerText = "Time: " + hour % 12 + " AM"
+                currentTemp.innerText = "Temperature: " + (newHour.temp * 9 / 5 + 32) + " F"
             }
 
-        };
-
-        for (let x = 0; x < hours.length; x++) {
-            newHour = ++hour
-            if (newHour == (hours[newHour].datetime.split(":")[0])) {
-                let newT = document.createElement("h4")
-                let newP = document.createElement("p")
-                container.append(newT)
-                container.append(newP)
-                if (newHour > 11 && newHour < 25) {
-                    newT.append("Time: ", hours[newHour].datetime.split(":")[0] % 12, " PM")
-                }
-                else if (newHour >= 0 && newHour < 12) {
-                    newT.append("Time: ", hours[newHour].datetime.split(":")[0] % 12, " AM")
-                }
-                newP.append("Temperature: ", hours[newHour].temp * 9 / 5 + 32 + " F")
+            else if (hour === newHour.datetime.split(":")[0] && hour <= 23) {
+                currentHour.innerText = "Time: " + hour % 12 + " PM"
+                currentTemp.innerText = "Temperature: " + (newHour.temp * 9 / 5 + 32) + " F"
             }
         }
+
+            // for (let x = 0; x < hours.length; x++) {
+            //     newHour = ++hour
+            //     if (newHour == (hours[newHour].datetime.split(":")[0])) {
+            //         let newT = document.createElement("h4")
+            //         let newP = document.createElement("p")
+            //         container.append(newT)
+            //         container.append(newP)
+            //         if (newHour > 11 && newHour < 25) {
+            //             newT.append("Time: ", hours[newHour].datetime.split(":")[0] % 12, " PM")
+            //         }
+            //         else if (newHour >= 0 && newHour < 12) {
+            //             newT.append("Time: ", hours[newHour].datetime.split(":")[0] % 12, " AM")
+            //         }
+            //         newP.append("Temperature: ", hours[newHour].temp * 9 / 5 + 32 + " F")
+            //     }
+            // }
+        }
     }
-}
 
-function weekly(data) {
-    
-    let dayOne = data.days[0].datetime.split("-")
-    document.getElementById("dayA").append(dayOne[1] + "/" + dayOne[2])
-    document.getElementById("dayATemp").append("Temperature: ", data.days[0].temp * 9 / 5 + 32 + " F")
-    document.getElementById("dayACond").append("Conditions: ", data.days[0].conditions)
+    function weekly(data) {
 
-    let dayTwo = data.days[1].datetime.split("-")
-    document.getElementById("dayB").append(dayTwo[1] + "/" + dayTwo[2])
-    document.getElementById("dayBTemp").append("Temperature: ", data.days[1].temp * 9 / 5 + 32 + " F")
-    document.getElementById("dayBCond").append("Conditions: ", data.days[1].conditions)
+        let dayOne = data.days[0].datetime.split("-")
+        document.getElementById("dayA").append(dayOne[1] + "/" + dayOne[2])
+        document.getElementById("dayATemp").append("Temperature: ", data.days[0].temp * 9 / 5 + 32 + " F")
+        document.getElementById("dayACond").append("Conditions: ", data.days[0].conditions)
 
-    let dayThree = data.days[2].datetime.split("-")
-    document.getElementById("dayC").append(dayThree[1] + "/" + dayThree[2])
-    document.getElementById("dayCTemp").append("Temperature: ", data.days[2].temp * 9 / 5 + 32 + " F")
-    document.getElementById("dayCCond").append("Conditions: ", data.days[2].conditions)
+        let dayTwo = data.days[1].datetime.split("-")
+        document.getElementById("dayB").append(dayTwo[1] + "/" + dayTwo[2])
+        document.getElementById("dayBTemp").append("Temperature: ", data.days[1].temp * 9 / 5 + 32 + " F")
+        document.getElementById("dayBCond").append("Conditions: ", data.days[1].conditions)
 
-    let dayFour = data.days[3].datetime.split("-")
-    document.getElementById("dayD").append(dayFour[1] + "/" + dayFour[2])
-    document.getElementById("dayDTemp").append("Temperature: ", data.days[3].temp * 9 / 5 + 32 + " F")
-    document.getElementById("dayDCond").append("Conditions: ", data.days[3].conditions)
+        let dayThree = data.days[2].datetime.split("-")
+        document.getElementById("dayC").append(dayThree[1] + "/" + dayThree[2])
+        document.getElementById("dayCTemp").append("Temperature: ", data.days[2].temp * 9 / 5 + 32 + " F")
+        document.getElementById("dayCCond").append("Conditions: ", data.days[2].conditions)
 
-    let dayFive = data.days[4].datetime.split("-")
-    document.getElementById("dayE").append(dayFive[1] + "/" + dayFive[2])
-    document.getElementById("dayETemp").append("Temperature: ", data.days[4].temp * 9 / 5 + 32 + " F")
-    document.getElementById("dayECond").append("Conditions: ", data.days[4].conditions)
+        let dayFour = data.days[3].datetime.split("-")
+        document.getElementById("dayD").append(dayFour[1] + "/" + dayFour[2])
+        document.getElementById("dayDTemp").append("Temperature: ", data.days[3].temp * 9 / 5 + 32 + " F")
+        document.getElementById("dayDCond").append("Conditions: ", data.days[3].conditions)
 
+        let dayFive = data.days[4].datetime.split("-")
+        document.getElementById("dayE").append(dayFive[1] + "/" + dayFive[2])
+        document.getElementById("dayETemp").append("Temperature: ", data.days[4].temp * 9 / 5 + 32 + " F")
+        document.getElementById("dayECond").append("Conditions: ", data.days[4].conditions)
 
 
-}
 
-function navButtons() {
-    let hourlyButton = document.getElementById("hourly")
-    let weeklyButton = document.getElementById("weekly")
-    let container = document.getElementById("containerB")
-    let container1 = document.getElementById("containerC")
-    let title = document.getElementById("forecast")
-    let title1 = document.getElementById("weeklyForecast")
-    weeklyButton.addEventListener("click", () => {
-        title1.style.visibility = "visible";
-        title.style.visibility = "hidden";
-        container1.style.visibility = "visible";
-        container.style.visibility = "hidden";
+    }
 
-
-        hourlyButton.addEventListener("click", () => {
-            title1.style.visibility = "hidden";
-            title.style.visibility = "visible";
-            container1.style.visibility = "hidden";
-            container.style.visibility = "visible";
+    function navButtons() {
+        let hourlyButton = document.getElementById("hourly")
+        let weeklyButton = document.getElementById("weekly")
+        let container = document.getElementById("containerB")
+        let container1 = document.getElementById("containerC")
+        let title = document.getElementById("forecast")
+        let title1 = document.getElementById("weeklyForecast")
+        weeklyButton.addEventListener("click", () => {
+            title1.style.visibility = "visible";
+            title.style.visibility = "hidden";
+            container1.style.visibility = "visible";
+            container.style.visibility = "hidden";
 
 
+            hourlyButton.addEventListener("click", () => {
+                title1.style.visibility = "hidden";
+                title.style.visibility = "visible";
+                container1.style.visibility = "hidden";
+                container.style.visibility = "visible";
+
+
+            })
         })
-    })
-}
+    }
+
+// function weatherBG(data) {
+//     console.log(data)
+//     let currentCondition = data.currentConditions.conditions
+//     if (currentCondition.innerText = "overcast") {
+//         document.getElementById("navBar").style.backgroundImage =
+//             "url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Overcast_skies_from_Tropical_Storm_Danny.jpg/1200px-Overcast_skies_from_Tropical_Storm_Danny.jpg?20091209195310')"
+//     }
+//     else {
+
+//     }
+// }
